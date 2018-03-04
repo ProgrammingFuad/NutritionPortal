21ae46e9599f2fd51d03292450e22a7b3b0f1dbf
Fuad Mohamoud <fuad.mohamoud@und.edu>
2018-03-03 19:11:20 -0600

    Updated Angular Version
    Had to update the versions on all NPM packages

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material';
-import {MatToolbarModule} from '@angular/material';
+import { MatButtonModule,MatToolbarModule } from '@angular/material';
+
 
 @NgModule({
     imports: [MatButtonModule, MatToolbarModule],
