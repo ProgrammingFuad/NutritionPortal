e0e0d285600067aac4fa549680ac0c02f77c622c
Fuad Mohamoud <fuad.mohamoud@und.edu>
2018-05-09 02:20:00 -0500

    Update to Messaging & title

--- src/app/messaging/messageService.service.ts
+++ src/app/messaging/messageService.service.ts
@@ -16,32 +16,16 @@ export class messageService {
   
 //    this.newsFeedService.addItem(itemTitle.value,itemDescription.value,itemURL,unixTime);
   sendMessage(message: String ,time: number, type:string){
-    /*
-    message= message.trim();
-    this.firebase.database.ref('conversations/' + "FW1Qik9nE3ZLiC7Xk9fgBcabUwm1").set({
-      content: message,
-      timeStamp: time,
-      type: type
-    });
-    
-    this.convos.push({
-      content: message,
-      timeStamp: time,
-      type: type
-    });
-  }
-  */
- message = message.trim();
-   var number= Math.random() *1000000;
-   number= Math.floor(number);
-   var myNumber= String(number);
- var usersRef = this.firebase.database.ref("conversations");
-   usersRef.child("FW1Qik9nE3ZLiC7Xk9fgBcabUwm1").child(myNumber).set({ 
-    content: message,
-    timeStamp: time,
-    type: type
-   });
- 
+        message = message.trim();
+        var number= Math.random() *1000000;
+        number= Math.floor(number);
+        var myNumber= String(number);
+        var usersRef = this.firebase.database.ref("conversations");
+        usersRef.child("FW1Qik9nE3ZLiC7Xk9fgBcabUwm1").child(myNumber).set({ 
+          content: message,
+          timeStamp: time,
+          type: type
+        });
  }
 
 }
